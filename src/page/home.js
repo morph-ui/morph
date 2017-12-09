0122e444620cd2f694f56a37cf3d180001451ad4
techfano <mixc21@gmail.com>
2017-12-08 19:25:06 -0500

    fixing

--- src/page/home.js
+++ src/page/home.js
@@ -11,15 +11,12 @@ export default class Home extends React.Component {
           <img src="https://image.ibb.co/iqOQ7F/morph_logo_mini_fw.png" alt="" />
         </p>
         <h1>
-          ¿Que es morph?
+          ¿Qué es morph?
         </h1>
         <div className="row font-18">
           <div className="eight columns padding-5 margin-10">
-            Morph es una colección de Patrones de Diseño Web
-            que sirven para crear Interfaces Grafícas de Usuario (GUI) lo más fácíl posible.
-            Las cuales, han sido creadas usando solamente HTML con un mínimo de clases CSS.
-            Asi mismo, se abstrae en el nombre de las clases de estilos CSS una semántica que
-            hace referencia a un lenguaje natural de acuerdo a lo que se quiere hacer o usar.
+          Morph es una colección de Patrones de Diseño Web, sirve para crear Interfaces Gráficas de Usuario (GUI del inglés Graphical User Interface) de la manera más sencilla posible. Ha sido creado usando sólo HTML y con un mínimo de clases CSS. Asimismo, se abstrae en el nombre de las clases de estilos CSS una semántica que hace referencia a un lenguaje natural de acuerdo a lo que se quiere hacer o usar.
+          Finalmente, todo ello permite mejorar la experiencia del diseñador, programador y del usuario.
           </div>
           <div className="four columns padding-5 margin-10"></div>
         </div>
@@ -37,7 +34,7 @@ export default class Home extends React.Component {
         </p>
         <p className="font-18">
           <a className="button green rounded"
-            href={`https://morph.pe/zip/morph-${ packageJson.version }`} download>
+            href={`https://morph.pe/zip/morph-${ packageJson.version }.zip`} download>
             Descargar v{packageJson.version}
           </a>
         </p>
@@ -46,14 +43,13 @@ export default class Home extends React.Component {
         <h3>1. Minimalismo</h3>
         <div className="row font-18">
           <div className="eight columns padding-5 margin-10">
-            Morph usa minimalismo como una prioridad para crear interfaces en su minima expresion de diseño,
-            detalles y elementos para incrementar eficiencia.
+            Morph usa minimalismo como una prioridad para crear interfaces en su mínima expresión de diseño, detalles y elementos para incrementar eficiencia.
           </div>
         </div>
         <h3>2. Formas</h3>
         <div className="row font-18">
           <div className="eight columns padding-5 margin-10">
-            Solo formas geometricas basicas como cuadrados, circulos y sus caracteristicas derivadas.
+            Sólo formas geométricas básicas como cuadrados, círculos y sus características derivadas.
           </div>
         </div>
       </div>
