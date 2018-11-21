cd8197e72c60ec1ffc22ecfac33887e2efb7ded1
Alexandro Estefano Castañeda Bello <ecastaneda@Mac-mini-de-ECASTANEDA.local>
2018-11-21 16:23:43 -0500

    version 2 fixing

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-for': {},
+    'postcss-each': {},
+    'postcss-simple-vars': {},
+    'postcss-conditionals': {},
+    'postcss-nested': {},
+    'postcss-extend': {},
+    'cssnano': {}
+  }
+}
